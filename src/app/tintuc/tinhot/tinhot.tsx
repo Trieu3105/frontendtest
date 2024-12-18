e1fd23c205b5e216ba1ea8b814c477d06a807feb
I don't know code <tmlnguyen3105@gmail.com>
2024-12-18 15:23:44 +0700

    update

--- src/app/tintuc/tinhot/tinhot.tsx
+++ src/app/tintuc/tinhot/tinhot.tsx
@@ -3,8 +3,8 @@
 import React from 'react';
 
 // Hàm lấy dữ liệu bài viết từ API
-const fetchPost = async (slug: string) => {
-  const res = await fetch(`http://localhost:8080/api/tintuc/${slug}`, {
+const fetchPost = async (id: string) => {
+  const res = await fetch(`http://localhost:8080/api/tintuc/tinhot/${id}`, {
     cache: 'no-store', // Tắt cache để lấy dữ liệu mới nhất
   });
 
@@ -15,7 +15,7 @@ const fetchPost = async (slug: string) => {
 };
 
 // Component chi tiết bài viết
-const PostDetail = async ({ params }: { params: { slug: string } }) => {
+const Tinhot = async ({ params }: { params: { slug: string } }) => {
   const { slug } = params; // Lấy slug từ params
 
   try {
@@ -30,6 +30,7 @@ const PostDetail = async ({ params }: { params: { slug: string } }) => {
         <p className='mt-4 text-sm text-gray-700'>Lượt xem: {post.views}</p>
       </div>
     );
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     // Xử lý lỗi nếu không tìm thấy bài viết
     return (
@@ -41,4 +42,4 @@ const PostDetail = async ({ params }: { params: { slug: string } }) => {
   }
 };
 
-export default PostDetail;
+export default Tinhot;
